e9e05c333c5bf10d5185b4b26cc8b50a36bd19d4
Hieu Nguyen <hieuchi911@gmail.com>
2024-07-17 18:47:32 -0700

    Separate data processing module from finetuning scri[ts

--- scripts/llama/tools/process_data_cnn_dailymail.sh
+++ scripts/llama/tools/process_data_cnn_dailymail.sh
@@ -1,16 +1,24 @@
-BASE_PATH=${1-"/home1/hieutn/cs566/i-am-sober"}
-MODEL_PATH=${2-"/scratch1/hieutn/hub/models--meta-llama--Meta-Llama-3-8B-Instruct/snapshots/e1945c40cd546c78e41f1151f4db032b271faeaa/"}
-MODEL_TYPE=${3-"llama"}
-export TF_CPP_MIN_LOG_LEVEL=3
+BASE_PATH="/home1/hieutn/cs566/i-am-sober" # path to i-am-sober folder
+# model
+MODEL_PATH="/scratch1/hieutn/hub/models--meta-llama--Meta-Llama-3-8B-Instruct/snapshots/e1945c40cd546c78e41f1151f4db032b271faeaa/"  # path to model snapshots
+MODEL_TYPE="llama"
+# length
+MAX_LENGTH=1024
+MAX_PROMPT_LENGTH=512
+# data
+DATA_DIR=${BASE_PATH}/processed_data/cnn_dailymail/full-${MAX_LENGTH}-${MAX_PROMPT_LENGTH}
+TRAIN_SIZE=15000
+DEV_NUM=1000
 
-# prompt and response for baselines
+# Tokenize data and save in binary files
 PYTHONPATH=${BASE_PATH} python ${BASE_PATH}/tools/process_data_cnn_dailymail.py \
+    --model-path ${MODEL_PATH} \
+    --model-type ${MODEL_TYPE} \
     --hugg-data-id abisee/cnn_dailymail \
     --hugg-data-subset 1.0.0 \
-    --processed-data-dir ${BASE_PATH}/processed_data/cnn_dailymail/full \
-    --model-path ${MODEL_PATH} \
+    --max-length ${MAX_LENGTH} \
+    --max-prompt-length ${MAX_PROMPT_LENGTH} \
+    --processed-data-dir ${DATA_DIR} \
     --data-process-workers 32 \
-    --max-prompt-length 3500 \
-    --max-length 4096 \
-    --dev-num 1000 \
-    --model-type ${MODEL_TYPE}
+    --train-size ${TRAIN_SIZE} \
+    --dev-num ${DEV_NUM}
